a0d7143989c3e4b5943a7bcf473124c33563a89c
Kartik Sharma <kartiksharna321@gmail.com>
2025-12-17 21:06:40 +0530

    ramdomness

--- thing.js
+++ thing.js
@@ -1,15 +1,21 @@
-console.log("2nd Task")
+console.log("3rd Task")
 
 let btn=document.querySelector("button")
-let div=document.querySelector("#mine")
  
 btn.addEventListener("click",function(){
 
    let c1=Math.floor(Math.random()*256)
    let c2=Math.floor(Math.random()*256)
    let c3=Math.floor(Math.random()*256)
-   div.style.backgroundColor=`rgb(${c1},${c2},${c3})`
-
+   let x=Math.random()*100
+   let y=Math.random()*100
+   let dick =document.createElement("div")
+   
+   dick.style.backgroundColor=`rgb(${c1},${c2},${c3})`
+   dick.style.left=y+"vw"
+   dick.style.top=x+"vh"
+   let body=document.querySelector("body")
+   body.append(dick)
 })
 
 
